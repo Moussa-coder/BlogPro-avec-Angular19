5b5641f492bd7eca760d54ce5ffd656be199ab03
Moussa-codeur <correamoussa469@gmail.com>
2025-05-25 23:08:56 +0200

    création des composants standalone Home, Login, Register

--- blog-angular-standalone/src/main.ts
+++ blog-angular-standalone/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+// Il joue un rôle crucial dans le démarrage de l'application Angular en initialisant
+// le module racine (généralement AppModule) et en "montant" (bootstrap) l'application dans le DOM.
